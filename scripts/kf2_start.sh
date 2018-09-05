0f79fb7562dcdd9807cadf3f78e57e10c9e9907e
bill.krause <bill.krause@lithium.com>
2018-09-04 18:35:08 -0700

    Change KF2 Server confs

--- scripts/kf2_start.sh
+++ scripts/kf2_start.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-screen -m -S ./Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab KF-Mario_Room +QueryPort=27019 +Multihome=192.168.1.8
+
+/tmp/steamcmd/steamcmd.sh +login anonymous +force_install_dir ~/kf2 +app_update 232130 +validate +quit ;
+screen -m -S KF2 ./Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab KF-Mario_Room +QueryPort=27019 +Multihome=192.168.1.8
