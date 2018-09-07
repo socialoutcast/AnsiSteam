284fb642d68a2e69953ff038ea31ccfa25dc2bfa
bill.krause <bill.krause@lithium.com>
2018-09-07 13:24:53 -0700

    Change KF2 Server startup

--- scripts/kf2_start.sh
+++ scripts/kf2_start.sh
@@ -2,4 +2,4 @@
 
 
 /tmp/steamcmd/steamcmd.sh +login anonymous +force_install_dir ~/kf2 +app_update 232130 +validate +quit ;
-screen -S KF2 -d -m ./Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab KF-Mario_Room +QueryPort=27019 +Multihome=192.168.1.8
+screen -S KF2 -d -m ./Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab KF-Mario_Room +QueryPort=27018 +Multihome=192.168.1.8
