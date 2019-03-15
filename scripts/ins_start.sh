ca8c067bb3d76e6874326b65c8752b36d63edf4d
socialoutcast <xf.imonaboat@gmail.com>
2019-03-14 17:25:29 -0700

    Added new Squad Settings

--- scripts/ins_start.sh
+++ scripts/ins_start.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+
+/tmp/steamcmd/steamcmd.sh +login anonymous +force_install_dir ~/sandstorm +app_update 581330 +validate +quit
 screen -m -S ins ./srcds_linux -console +map market_coop +maxplayers 8 -port 27020
