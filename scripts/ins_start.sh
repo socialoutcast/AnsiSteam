955a179440c802d565ef71a847cfdd9bddf9cebf
socialoutcast <xf.imonaboat@gmail.com>
2019-09-30 23:24:08 -0700

    added sandstorm

--- scripts/ins_start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-/tmp/steamcmd/steamcmd.sh +login anonymous +force_install_dir ~/sandstorm +app_update 581330 +validate +quit
-screen -m -S ins ./srcds_linux -console +map market_coop +maxplayers 8 -port 27020
