6f15023f728457ecae96b5ceee4d3d9d992c339c
socialoutcast <xf.imonaboat@gmail.com>
2021-03-04 22:03:49 -0800

    Removed private Data

--- scripts/kf2_start.sh
+++ scripts/kf2_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 /tmp/steamcmd/steamcmd.sh +login anonymous +force_install_dir ~/kf2 +app_update 232130 +quit ;
-screen -S KF2 -d -m ./Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab KF-Mario_Room +QueryPort=27018 +Multihome=192.168.1.8
+screen -S KF2 -d -m ./Binaries/Win64/KFGameSteamServer.bin.x86_64 kf-bioticslab KF-Mario_Room +QueryPort=27018 +Multihome=IP
--- scripts/squad_start.sh
+++ scripts/squad_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 /tmp/steamcmd/steamcmd.sh +login anonymous +force_install_dir ~/squad +app_update 403240 +validate +quit
-screen ./SquadServer.sh Port=7787 QueryPort=27165 FIXEDMAXPLAYERS=72 FIXEDMAXTICKRATE=50 RANDOM=ALWAYS MULTIHOME=192.168.1.11 -log
+screen ./SquadServer.sh Port=7787 QueryPort=27165 FIXEDMAXPLAYERS=72 FIXEDMAXTICKRATE=50 RANDOM=ALWAYS MULTIHOME=IP -log
