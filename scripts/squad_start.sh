b44591197383457ce77728dab290bcd74cff6a47
socialoutcast <xf.imonaboat@gmail.com>
2021-03-06 16:32:57 -0800

    updated all code o ref a single env in the servers,inv setup. Made changes to the Squad install to now completely install and config the setup.

--- scripts/squad_start.sh
+++ scripts/squad_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 /tmp/steamcmd/steamcmd.sh +login anonymous +force_install_dir ~/squad +app_update 403240 +validate +quit
-screen ./SquadServer.sh Port=7787 QueryPort=27165 FIXEDMAXPLAYERS=72 FIXEDMAXTICKRATE=50 RANDOM=ALWAYS MULTIHOME=IP -log
+screen ./SquadGameServer.sh Port=7787 QueryPort=27165 FIXEDMAXPLAYERS=72 FIXEDMAXTICKRATE=60 RANDOM=ALWAYS MULTIHOME=IP -log
